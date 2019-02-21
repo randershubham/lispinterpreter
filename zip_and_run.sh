3dafa4d4928662d415bb29099433d59ff7df01ed
srander <shubham.rander23@gmail.com>
2019-02-21 01:52:55 -0500

    Automating tests

--- zip_and_run.sh
+++ zip_and_run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 mkdir -p project
 find . \( -name '*.java' -or -name 'makefile' -or -name 'Runfile' -or -name 'input.txt' \) -exec cp {} ./project/  \;
-zip project.zip project
+zip -r project.zip project
 rm -rf project
 mv project.zip ~
+cd ~
+unzip project.zip
+make
+Runfile < input.txt
