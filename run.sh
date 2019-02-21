35e6ec5ed911b078bbe368cd4e241c44515a7082
srander <shubham.rander23@gmail.com>
2019-02-21 01:19:34 -0500

    Updating run file and run file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 find -name "*.java" > sources.txt
 javac @sources.txt
+find -name "*.class" -exec mv -t ./ {} +
 ./Runfile < ./src/main/resources/input.txt
 find -name "*.class" -exec rm -f {} \;
