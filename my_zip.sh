da3e3f5d65410318264b31910bb27b352f0746e3
srander <shubham.rander23@gmail.com>
2019-02-21 01:41:30 -0500

    Updating zip file

--- my_zip.sh
+++ my_zip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p project
-find . \( -name '*.java' -or -name 'makefile' -or -name 'Runfile' \) -exec cp {} ./project/  \;
+find . \( -name '*.java' -or -name 'makefile' -or -name 'Runfile' -or -name 'input.txt' \) -exec cp {} ./project/  \;
 zip project.zip project
+rm -rf project
+mv project.zip ~
